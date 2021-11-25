919c7934bd283edc79c104cf61fce5a84d70e0ea
akbarfach <akbar.fach77@gmail.com>
2021-11-25 12:02:53 +0700

    TA_MDP

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1 className="title">About</h1>
+      <div className="about-container">
+        <img src="/akbar-f.jpg" alt="akbar pic" />
+        <p>Nama: Mochamad Akbar Fachrezy</p>
+        <p>NIM: 21120119120029</p>
+        <div className="about-app">
+          <h2 className="title">About Football Corner</h2>
+          <p>
+            Football Corner dibuat untuk memenuhi tugas akhir Praktikum MDP.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
